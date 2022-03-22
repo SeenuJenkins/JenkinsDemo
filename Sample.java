b9f30ff4506705883604cde44f7fada20f120448
SeenuJenkins <102139508+SeenuJenkins@users.noreply.github.com>
2022-03-22 16:22:14 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,67 @@
+package roughwork;
+
+public class RichardWEtask1 {
+	private float Tamil, English, Maths, Science, Social, Total,sort;
+
+	
+	public float getSort() {
+		
+		
+		return sort;
+	}
+
+	public void setSort(float sort) {
+		this.sort = sort;
+	}
+
+	public float getTamil() {
+		return Tamil;
+	}
+
+	public void setTamil(float tamil) {
+		this.Tamil = tamil;
+	}
+
+	public float getEnglish() {
+		return English;
+	}
+
+	public void setEnglish(float english) {
+		this.English = english;
+	}
+
+	public float getMaths() {
+		return Maths;
+	}
+
+	public void setMaths(float maths) {
+		this.Maths = maths;
+	}
+
+	public float getScience() {
+		return Science;
+	}
+
+	public void setScience(float science) {
+		this.Science = science;
+	}
+
+	public float getSocial() {
+		return Social;
+	}
+
+	public void setSocial(float social) {
+		this.Social = social;
+	}
+	
+	public float getTotal() {
+		
+		return Total=Tamil+English+Maths+Science+Social;
+	}
+
+	public void setTotal(float total) {
+		this.Total = total;
+	}
+
+
+}
